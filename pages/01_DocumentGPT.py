7dbb8ea8e314e39f6a984782a0e3fcd6b64780e0
hijunglit <hijunglit@gmail.com>
2025-05-22 18:21:52 +0900

    Chain

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,12 +1,24 @@
-import time
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
+st.set_page_config(
+    page_title="Document GPT",
+    page_icon="📃"
+)
 
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -19,7 +31,7 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    loader = UnstructuredFileLoader(file_path)
 
     docs = loader.load_and_split(text_splitter=splitter)
 
@@ -34,14 +46,73 @@ def embed_file(file):
     return retriever
 
 
+def send_massage(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_massage(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+
+            Context: {context}
+            """
+        ),
+        ("human", "{question}")
+    ]
+)
+
 st.title("DocumentGPT")
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
+st.markdown(
+    """
+    Welcome!
+            
+    Use this chatbot to ask question to an AI about your files :) !
+            
+    Upload your files on the sidebar.
+"""
 )
+
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+
+    send_massage("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_massage(message, "human")
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm
+        response = chain.invoke(message)
+        send_massage(response.content, "ai")
+
+
+else:
+    st.session_state["messages"] = []
