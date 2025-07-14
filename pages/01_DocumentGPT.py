a5de9fb0824dcb880457e3392e66c3079db70ae7
hijunglit <hijunglit@gmail.com>
2025-07-14 23:35:11 +0900

    chain

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,15 +1,19 @@
-import streamlit as st
-import time
+from langchain.prompts import ChatPromptTemplate
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
 
 st.set_page_config(
     page_title="FullstackGPT Home",
     page_icon="📃"
 )
+
+llm = ChatOpenAI(temperature=0.1)
 
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -50,6 +54,25 @@ def paint_history():
         )
 
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """
+        ),
+        ("human", "{question}")
+    ]
+)
+
+
 st.title("DocumentGPT")
 
 st.markdown("""
@@ -71,6 +94,16 @@ if file:
     message = st.chat_input("Ask anything about your file...")
     if message:
         send_message(message, "human")
-        send_message("lalala", "ai")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough()
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
 else:
     st.session_state["messages"] = []
