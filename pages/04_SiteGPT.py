464bdb1ce179f1cdbd43247853f22452345c2400
hijunglit <hijunglit@gmail.com>
2025-07-24 18:51:32 +0900

    parsing function

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,10 +1,27 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
+
+splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+
+
+def page_parse(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return str(soup.get_text()).replace("\n", " ").replace("\xa0", " ")
 
 
 @st.cache_data(show_spinner="Loading Website...")
 def load_websites(docs):
-    loader = SitemapLoader(url)
+    loader = SitemapLoader(
+        url, filter_urls=[r"^(.*\/blog\/).*"], parsing_function=page_parse)
     loader.requests_per_second = 5
     docs = loader.load()
     return docs
@@ -37,3 +54,4 @@ if url:
             st.error("Please write down a sitemap URL")
     else:
         docs = load_websites(url)
+        st.write(docs)
