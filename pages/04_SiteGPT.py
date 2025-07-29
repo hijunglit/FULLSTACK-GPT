039562ec31b054ee2e55256785a78094bf099dc3
hijunglit <hijunglit@gmail.com>
2025-07-29 17:07:34 +0900

    siteGPT

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,11 +1,100 @@
 from langchain.document_loaders import SitemapLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-4.1-nano-2025-04-14",
+    max_tokens=2048
+)
+
+answers_prompt = ChatPromptTemplate.from_template("""
+     Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+""")
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    # answers = []
+    # for doc in docs:
+    #     result = answers_chain.invoke({
+    #         "question": question,
+    #         "context": doc.page_content
+    #     })
+    #     answers.append(result.content)
+    return {"question": question,
+            "answers": [
+                {
+                    "answer": answers_chain.invoke(
+                        {"question": question, "context": doc.page_content}
+                    ).content,
+                    "source": doc.metadata["source"],
+                }
+                for doc in docs
+            ],
+            }
+
 
 splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size=1000,
     chunk_overlap=200,
 )
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to answer the user's question.
+
+            Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
+            Return the sources of the answers as they are, do not change them.
+
+            Answers: {answers}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs):
+    answers = inputs["answers"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(
+        f"{answer['answer']}\nSource:{answer['source']}\n" for answer in answers)
+    return choose_chain.invoke({"question": question, "answers": condensed, })
 
 
 def page_parse(soup):
@@ -21,10 +110,12 @@ def page_parse(soup):
 @st.cache_data(show_spinner="Loading Website...")
 def load_websites(docs):
     loader = SitemapLoader(
-        url, filter_urls=[r"^(.*\/blog\/).*"], parsing_function=page_parse)
+        url, parsing_function=page_parse)
     loader.requests_per_second = 5
     docs = loader.load()
-    return docs
+    print(docs)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 
 st.set_page_config(
@@ -53,5 +144,16 @@ if url:
         with st.sidebar:
             st.error("Please write down a sitemap URL")
     else:
-        docs = load_websites(url)
-        st.write(docs)
+        retriever = load_websites(url)
+        query = st.text_input("Ask a question to the website")
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+            result = chain.invoke(query)
+            st.write(result.content)
